5a41017f3b1bea29e0981f7702ebcafd03d0a881
pardue95 <85760640+pardue95@users.noreply.github.com>
2022-01-30 13:05:12 -0600

    test passed for Nav and links

--- photo-port/src/components/About/__tests__/index.test.js
+++ photo-port/src/components/About/__tests__/index.test.js
@@ -1,6 +1,6 @@
- import React from 'react';
- import { render, cleanup } from '@testing-library/react';
- import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
 afterEach(cleanup);
@@ -19,4 +19,4 @@ describe('About component', () => {
 })
   
   
-})
+});
